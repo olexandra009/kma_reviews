f124bf1618679c9723071d48ec3816f86e3ef337
Alexandra <a.r.chumachenko@gmail.com>
2021-02-19 14:54:55 +0200

    Configure webpack

---
+++ webpack.common.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+    ],
+    output: {
+        filename: 'js/main.js',
+        path: path.resolve(__dirname, 'docs'),
+    },
+};
